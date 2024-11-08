fedb92496243720621004ffc767514adc62eca09
NguyenMinhNguyen1706 <minhnguyenn786@gmail.com>
2024-11-08 10:44:12 +0700

    Update and rename Exercise3_MultiplicationTable.java to Assignment5_3.java

--- Assignment5_3.java
+++ Assignment5_3.java
@@ -1,6 +1,7 @@
+// Exercise 3: Multiplication Table
+// Description: Write a program that generates and prints the multiplication table for a number up to 10.
 import java.util.Scanner;
 public class Exercise3_MultiplicationTable {
-    
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int number;
